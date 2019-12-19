6edfb52ec4c9b821cee3f51c82dde41702e9ac63
keremsabirli <keremsabirli15@gmail.com>
2019-12-19 23:51:48 +0300

    changed connectionstring to devconnection string

--- SchoolProjectAPI/Startup.cs
+++ SchoolProjectAPI/Startup.cs
@@ -31,9 +31,9 @@ namespace SchoolProjectAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<SchoolProjectContext>(opts => opts.UseSqlServer(Configuration["ConnectionString:SchoolProjectDB"]));
+            services.AddDbContext<SchoolProjectContext>(opts => opts.UseSqlServer(Configuration["ConnectionString:prodSchoolProjectDB"]));
             services.AddControllers();
-            var config = new AutoMapper.MapperConfiguration(cfg =>
+            var config = new MapperConfiguration(cfg =>
             {
                 cfg.AddProfile(new MapperProfile());
             });
